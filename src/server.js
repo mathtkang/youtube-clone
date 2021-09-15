0e49e2513e230eb7c3ca75a53daa84f3ed89e3f5
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-15 15:14:07 +0900

    middleware part Two

--- src/server.js
+++ src/server.js
@@ -4,8 +4,17 @@ const PORT = 4000;
 
 const app = express(); //creates an express application
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const handleProtected = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue.");
     next();
 };
 
@@ -13,20 +22,28 @@ const handleHome = (req, res) => {
     //responses를 종료시키는 방법 2가지(end, send)
     return res.send("I love middlewares");
 };
-const handleAbout = (req, res) => {
-    return res.send("About here.");
-};
-const handleContact = (req, res) => {
-    return res.send("Contact here.");
-};
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
+// const handleAbout = (req, res) => {
+//     return res.send("About here.");
+// };
+// const handleContact = (req, res) => {
+//     return res.send("Contact here.");
+// };
+// const handleLogin = (req, res) => {
+//     return res.send("Login here.");
+// };
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
 };
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
+
+
+
+app.get("/", logger, handleHome);
+// app.get("/about", handleAbout);
+// app.get("/contact", handleContact);
+// app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
+
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
