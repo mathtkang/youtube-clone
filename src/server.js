ff539f8d3244bf2b350f7a1627e4473d23d6568b
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-15 14:10:56 +0900

    create route, day 2 of 43

--- src/server.js
+++ src/server.js
@@ -4,9 +4,26 @@ const PORT = 4000;
 
 const app = express(); //creates an express application
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => {
+    console.log(req);
+    console.log(res);
+    // return res.end(); //responses를 종료시키는 방법 2가지(end, send)
+    return res.send("I still love you.");
+};
+const handleAbout = (req, res) => {
+    return res.send("About here.");
+};
+const handleContact = (req, res) => {
+    return res.send("Contact here.");
+};
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
 
 app.get("/", handleHome);
+app.get("/about", handleAbout);
+app.get("/contact", handleContact);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
