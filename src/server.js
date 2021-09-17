2340c4c715e4368ddf9f785685d4e6b86aa759d6
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-18 01:17:14 +0900

    cleaning the code

--- src/server.js
+++ src/server.js
@@ -1,31 +1,14 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 app.use(logger)
-
-// global router
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-// users router
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-// videos router
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
