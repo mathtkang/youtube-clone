0d413543fd4c2dc1e2b5a5cab0782085eb041339
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-18 01:06:31 +0900

    feat global, users, videos router

--- src/server.js
+++ src/server.js
@@ -4,18 +4,26 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    return res.send("hello");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 app.use(logger)
-app.get("/", home);
-app.get("/login", login);
+
+// global router
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+// users router
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+// videos router
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
