da65c0522b62769a3228de38ba4daf7e4ae3a7aa
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-18 01:07:19 +0900

    feat connect router

--- src/server.js
+++ src/server.js
@@ -11,19 +11,26 @@ const globalRouter = express.Router();
 
 const handleHome = (req, res) => res.send("Home");
 
+globalRouter.get("/", handleHome);
+
 // users router
 const userRouter = express.Router();
 
 const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
 
 // videos router
 const videoRouter = express.Router();
 
 const handleWatchVideo = (req, res) => res.send("Watch Video");
 
+videoRouter.get("/watch", handleWatchVideo);
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+
 
 const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
